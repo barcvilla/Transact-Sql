f2548a9a195fdafaa341c522248499453dcb69e0
Carlos Villanueva <ceva_19@hotmail.com>
2017-12-05 16:00:37 -0300

    USING BUILT-IN FUNCTIONS AND EXPRESSIONS

--- ch04/USING BUILT-IN FUNCTIONS AND EXPRESSIONS.sql
+++ ch04/USING BUILT-IN FUNCTIONS AND EXPRESSIONS.sql
@@ -30,3 +30,27 @@ SELECT CONCAT(AddressLine1, ' ', PostalCode) as Address FROM Person.Address;
 SELECT CONCAT('I', ' ', 'love', ' ', 'using', ' ', 'CONCAT', ' ', 'because', ' ', 'NULL', 
 ' ', 'values', ' ', 'vanish', ' ', 'SEE:', NULL, '!') AS RESULT;
 
+/*
+* Hay dos funciones disponibles para reemplazar valores null con otros valores. ISNULL, el cual
+* requiere dos parametros: el valor a verificar y el reemplazo para valores null. La otra funcion es
+* COALESCE la cual tomara algun parametro y retorna el primero valor no Nulo. Los desarrolladores
+* T-Sql prefieren usar COALESCE sobre ISNULL porque COALESCE conoce el standard ANSI mientras que 
+* ISNULL no.
+* SINTAXIS:
+* ISNULL(<value>,<replacement>)
+* COALESCE(<value1>,<value2>,...,<valueN>)
+*/
+--3. ISNULL and COALESCE
+
+--3.1 Añade un espacio en blanco despues de FirstName y uno en blanco antes de LastName
+SELECT BusinessEntityID, FirstName + ' ' + ISNULL(MiddleName, '') + ' ' + LastName as "Full Name"
+FROM   Person.person;
+
+--3-2 Solucionamos el doble espacio entre FirstName y LastName producido en el query 3.1
+SELECT BusinessEntityID, FirstName + ISNULL(' ' + MiddleName, '') + ' ' + LastName as "Full Name"
+FROM   Person.person;
+
+--3.3 Si MiddleName es NULL, el proximo valor no null, la cadena vacia  es retornado.
+SELECT BusinessEntityID, FirstName + COALESCE(' ' + MiddleName, '') + ' ' + LastName as "Full Name"
+FROM   Person.person;
+
