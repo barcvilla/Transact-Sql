25e876787bfe679ceb8c4ad165315df123ffac39
Carlos Villanueva <ceva_19@hotmail.com>
2017-12-05 14:44:02 -0300

    USING BUILT-IN FUNCTIONS AND EXPRESSIONS

--- ch04/USING BUILT-IN FUNCTIONS AND EXPRESSIONS.sql
+++ ch04/USING BUILT-IN FUNCTIONS AND EXPRESSIONS.sql
@@ -1,16 +1,32 @@
 -- USING BUILT-IN FUNCTIONS AND EXPRESSIONS
 
---******* Concatenating Strings *******
--- 1.
+--1. ******* Concatenating Strings *******
+-- 1.1
 SELECT 'ab' + 'c';
 
--- 2. 
+-- 1.2. 
 SELECT BusinessEntityID, FirstName + ' ' + LastName as "Full Name" FROM Person.Person;
 
--- 3.
+-- 1.3.
 SELECT BusinessEntityID, LastName + ', ' + FirstName as "Full Name" FROM Person.Person;
 
--- ****** Concatenating Strings and Null *******
+--2. ****** Concatenating Strings and Null *******
 -- Cuando concatenamos un Sting con null, null es retornado. El sgte codigo muestra el problema
 SELECT BusinessEntityId, FirstName + ' ' + MiddleName + ' ' + LastName as "Full Name" FROM 
 	   Person.person;
+
+--2.1 Simple CONCAT function
+SELECT CONCAT('I', ' Love', ' writing', ' T-SQL') AS RESULT;
+
+--2.2 Usando variables con CONCAT
+DECLARE @a VARCHAR(30) = 'My birthday is on';
+DECLARE @b DATE = '1980/08/25';
+SELECT CONCAT(@a, @b) AS RESULT;
+
+--2.3 Usando CONCAT con filas de tablas
+SELECT CONCAT(AddressLine1, ' ', PostalCode) as Address FROM Person.Address;
+
+--2-4 Usando CONCAT con NULL
+SELECT CONCAT('I', ' ', 'love', ' ', 'using', ' ', 'CONCAT', ' ', 'because', ' ', 'NULL', 
+' ', 'values', ' ', 'vanish', ' ', 'SEE:', NULL, '!') AS RESULT;
+
